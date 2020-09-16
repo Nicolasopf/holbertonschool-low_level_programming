b09fa560ec28a3343c1ae28488dd2dbee33bfa94
Nico Felipe <nico15935746@gmail.com>
2020-09-16 19:06:12 +0000

    6 fixed

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-/*
-* main - Entry point, prints all sizeof
-*
-* Return: Always 0
-*/
+/**
+ * main - Entry point, prints all sizeof
+ *
+ * Return: Always 0
+ */
 
 int main(void)
 {
