5fdc5d1039cd3c5b04b939457c5b0ae5f51f8ba6
Nico Felipe <nico15935746@gmail.com>
2020-09-17 20:43:57 +0000

    9 fix

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,13 +10,14 @@ int main(void)
 	int c;
 		do {
 			for (c = 0; c <= 9; c++)
-				putchar(c + 48);
-			if (c != 9)
 			{
-				putchar(',')
-					putchar(' ');
-			}
-		} while (c < 9);
-	putchar('\n');
-	return (0);
+				putchar(c + 48);
+				if (c != 9)
+				{
+					putchar(',')
+						putchar(' ');
+				}
+			} while (c < 9);
+			putchar('\n');
+			return (0);
 }
