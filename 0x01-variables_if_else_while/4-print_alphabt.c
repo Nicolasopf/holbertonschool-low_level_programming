fd264ead32a38486b9491272f85262416465924a
Nico Felipe <nico15935746@gmail.com>
2020-09-17 17:51:57 +0000

    4 added

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Alphabet without q and e
+ */
+
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+
+		if (c == 'e' || c == 'q')
+		{
+			c++;
+		}
+
+		putchar(c);
+		c++;
+
+		if (c > 'z')
+		{
+			putchar('\n');
+		}
+	}
+
+	return (0);
+}
