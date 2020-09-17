7736fc3f0850272a2d82b7c77a9e85122fa08b84
Nico Felipe <nico15935746@gmail.com>
2020-09-17 21:33:27 +0000

    10 added

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+
+int c = 48;
+
+int b = 48;
+
+while (c <= 57)
+{
+for ( ; b <= 57; b++)
+{
+putchar(c);
+putchar(b);
+if (b == 57 && c == 57)
+{
+putchar('\n');
+break;
+}
+putchar(',');
+putchar(' ');
+}
+b = 48;
+c++;
+}
+return (0);
+}
