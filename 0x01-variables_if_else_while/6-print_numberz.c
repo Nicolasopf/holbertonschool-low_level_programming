76136708a9c692649e19cc53e97953fe2a9d990b
Nico Felipe <nico15935746@gmail.com>
2020-09-17 19:26:19 +0000

    6 fix

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: numbers
+ *
+ */
+
+int main(void)
+{
+	int n;
+
+	do {
+		for (n = 0; n <= 9; n++)
+			putchar(n + 48);
+	}
+	while (n < 9);
+	putchar('\n');
+return (0);
+}
