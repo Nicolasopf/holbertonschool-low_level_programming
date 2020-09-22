430e69350eda08bc6e5289389e6a88e6a04baf39
Nico Felipe <nico15935746@gmail.com>
2020-09-22 21:57:47 +0000

    9 added

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+ *times_table - prints the 9 table
+ */
+
+void times_table(void)
+{
+	int m, l, r;
+
+	for(m = 0; m <=9; m++)
+	{
+		for (l = 0; l <= 0; l++)
+		{
+			r = m * l;
+
+			if (r < 10)
+			{
+				if(l > 0)
+				{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				}
+				_putchar(r + '0');
+			}
+
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(r / 10 + '0');
+				_putchar (r % 10 + '0');
+			}
+		}
+		_putchar ('\n');
+	}
+}
