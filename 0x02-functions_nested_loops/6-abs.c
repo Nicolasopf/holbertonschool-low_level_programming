67c260b37e5a29efd2f5c0858f764c9fab62a9b9
Nico Felipe <nico15935746@gmail.com>
2020-09-22 20:48:59 +0000

    6 added

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ *_abs - convert into absolute
+ *@j: var
+ *Return: absolute value
+ */
+
+int _abs(int j)
+{
+	if (j < 0)
+	{
+		j = j * -1;
+		return (j);
+	}
+	else
+		return (j);
+}
