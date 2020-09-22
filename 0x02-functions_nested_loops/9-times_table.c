163653476b27d0f1ccf02454dabbcb6a71a29f79
Nico Felipe <nico15935746@gmail.com>
2020-09-22 22:05:42 +0000

    9 fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,29 +9,29 @@ void times_table(void)
 {
 	int m, l, r;
 
-	for (m = 0; m <= 9; m++)
+	for (m = 0; m < 10; m++)
 	{
-		for (l = 0; l <= 9; l++)
+		for (l = 0; l < 10; l++)
 		{
 			r = m * l;
 
-			if (r < 10)
+			if (r / 10 != 0)
 			{
-				if (l > 0)
+				if (l != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar(' ');
 				}
-				_putchar(r + '0');
+				_putchar(r / 10 + '0');
+				_putchar(r % 10 + '0');
 			}
 
 			else
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(r / 10 + '0');
-				_putchar(r % 10 + '0');
+				_putchar(' ');
+				_putchar(r + '0');
 			}
 		}
 		_putchar ('\n');
