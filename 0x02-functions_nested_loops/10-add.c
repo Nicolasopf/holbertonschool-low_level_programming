e5fb2ac8f84d55e5d9792ea1573e911d9e61457a
Nico Felipe <nico15935746@gmail.com>
2020-09-22 22:46:51 +0000

    10 added

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ *add - add two numbers
+ *@a: integer 1
+ *@b: integer 2
+ *Return: addition of two integers in vars
+ */
+
+nt add(a, b)
+{
+	int c;
+
+	c = a + b;
+	return (s);
+}
