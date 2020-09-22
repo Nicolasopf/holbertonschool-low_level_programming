d2a1e8a29d02a7abafd510d629658486ebc93858
Nico Felipe <nico15935746@gmail.com>
2020-09-22 19:47:22 +0000

    3 fix

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,5 +1,13 @@
 #include "holberton.h"
 
+/**
+ * _islower - check lower case
+ *
+ * @c: character
+ *
+ * Return: 1 lower case, 0 otherwise
+ */
+
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
