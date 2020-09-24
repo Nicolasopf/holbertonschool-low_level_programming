b2656d26897ee1d4e89c293d3235bd3ebe61afa5
Nico Felipe <nico15935746@gmail.com>
2020-09-24 23:21:03 +0000

    3 fix

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,11 +7,12 @@
 
 void print_numbers(void)
 {
-	int a;
 
-	for (a = 48; a < 58; a++)
-	{
-		_putchar(a);
-	}
-	_putchar('\n');
+int a;
+
+for (a = '0'; a < '9'; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -2,9 +2,11 @@
 #define HOLBERTON_H
 #include <stdio.h>
 
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
-int _putchar(char c);
+void print_numbers(void);
+void print_most_numbers(void);
 
 #endif /* HOLBERTON_h */
