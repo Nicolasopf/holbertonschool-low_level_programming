6c7edd0147a508120dcd9d0a766f6779af110e84
Nico Felipe <nico15935746@gmail.com>
2020-09-24 16:13:11 +0000

    3 added

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplies 'a' and 'b'
+ * @a: number to multiply 1
+ * @b: number to multiply 2
+ * @c: result in a int
+ * Return: result a mul b
+ */
+
+int mul(int a, int b)
+{
+
+	int c = a * b;
+
+	return (c);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ *
+ *
+ *
+ */
+
+void print_numbers(void)
+{
+
+	char a;
+
+	while (a >= '0' && a <= '9')
+	{
+		a++;
+		_putchar(a);
+	}
+}
