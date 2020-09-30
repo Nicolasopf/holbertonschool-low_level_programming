65349b9f17920e822c2f7cf2b56430242ae67990
Nico Felipe <nico15935746@gmail.com>
2020-09-30 02:52:27 +0000

    5 added

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * rev_string - reverse string
+ * @s: var char
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+	int j;
+	char a, b;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	i--;
+
+	for (j = 0; j <= i; j++)
+	{	 
+		a = s[j];
+		b = s[i];
+		s[i] = a;
+		s[j] = b;
+		i--;
+	}
+}
