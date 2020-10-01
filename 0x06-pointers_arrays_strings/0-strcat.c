e81e4eb50f8dee9588a4736599c9f61b8e0a7f3a
Nico Felipe <nico15935746@gmail.com>
2020-10-01 20:45:53 +0000

    0 added

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * _strcat - string concatenate
+ * @dest: destination
+ * @src: source
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+	int h;
+	char a;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++
+	}
+	dest[i] = '\0';
+	return (dest);
+}
