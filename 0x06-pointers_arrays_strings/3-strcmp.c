7d637dff6ae69e2e3fabd25e065960fb002da69b
Nico Felipe <nico15935746@gmail.com>
2020-10-02 04:56:15 +0000

    3 added

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,22 +1,20 @@
-  
 #include "holberton.h"
 
 /**
- * _strcmp - difference between strings
- * @s1: string 1
- * @s2: string 2
- * Return: comparationr
+ * _strcmp - compares two strings
+ * @s1: string to compare
+ * @s2: string to compare
+ *
+ * Return: comparation of strings
  */
 
 int _strcmp(char *s1, char *s2)
 {
-
 	int i = 0;
-	int j, c;
+	int j, cmp;
 
 	while (s1[i] != '\0')
 		i++;
-
 	for (j = 0; j < i; j++)
 	{
 		if (s1[j] == '\0' && s2[j] == '\0')
@@ -24,6 +22,6 @@ int _strcmp(char *s1, char *s2)
 		if (s1[j] != s2[j] || s2[j] == '\0')
 			break;
 	}
-	c = s1[j] - s2[j];
+	cmp = s1[j] - s2[j];
 	return (cmp);
 }
