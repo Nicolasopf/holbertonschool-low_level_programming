92b1f34ece0abd96d69196a1358b5ec96f125bec
Nico Felipe <nico15935746@gmail.com>
2020-10-06 02:19:00 +0000

    3 added

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ *
+ *
+ *
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int a;
+	int b;
+	unsigned int c;
+
+	for (a = 0; s[a] != '\0' && s[a] != ' '; a++)
+	{
+		for (b = 0; accept[b] != '\0'; b++)
+		{
+			if (s[a] == accept[b])
+			{
+				c++;
+			}
+		}
+	}
+	return (c);
+}
