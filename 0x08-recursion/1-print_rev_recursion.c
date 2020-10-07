d0a490d5590eba63d42256556c1162fe5172f510
Nico Felipe <nico15935746@gmail.com>
2020-10-07 20:12:34 +0000

    1 added

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - print a string in reverse
+ * @s: string
+ */
+
+void _print_rev_recursion(char *s)
+{
+	int i = 0;
+	if (s[i])
+	{
+		_print_rev_recursion(s + (i + 1));
+		_putchar(s[i]);
+	}
+}
