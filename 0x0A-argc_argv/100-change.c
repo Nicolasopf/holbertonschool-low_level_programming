f89d1be5ae5d6c5f794c74c03a642b61bc79083f
root <root@ip-172-31-45-8.us-east-2.compute.internal>
2020-10-11 23:40:26 +0000

    5 added

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - entry point
+ *@a: num of args
+ *@b: pointer to args
+ *Return: 0
+ */
+
+int main(int a, char **b)
+{
+	int sum, coins = 0;
+
+	if (a != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	sum = atoi(*(b + 1));
+	while (sum >= 25)
+		sum -= 25, coins++;
+	while (sum >= 10)
+		sum -= 10, coins++;
+	while (sum >= 5)
+		sum -= 5, coins++;
+	while (sum >= 2)
+		sum -= 2, coins++;
+	while (sum >= 1)
+		sum--, coins++;
+	printf("%d\n", coins);
+	return (0);
+}
