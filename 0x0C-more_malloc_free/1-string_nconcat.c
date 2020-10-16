0e24d49afbadfefde5dfec5683e7a6407211f931
root <root@ip-172-31-45-8.us-east-2.compute.internal>
2020-10-16 04:38:14 +0000

    1 fixed

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -28,7 +28,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	else
 		s2 = "";
-	if (n >= l2)
+	if (n >= len2)
 	{
 		con = malloc((sizeof(char) * (len1 + len2)) + 1);
 		if (con == NULL)
