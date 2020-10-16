4dd9f1a90d065062c518467534b02b3a42c6f905
root <root@ip-172-31-45-8.us-east-2.compute.internal>
2020-10-16 04:41:32 +0000

    0 fix

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,19 +1,19 @@
-#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "holberton.h"
 
 /**
- * malloc_checked - function that allocates memory using malloc.
- * @b: size.
- * Return: Always 0.
+ * malloc_checked - allocates memory using malloc.
+ * @b: bytes
+ * Return: 98 fail, pointer to allocated memory in succes.
  */
+
 void *malloc_checked(unsigned int b)
 {
-	void *a;
+	void *mem;
 
-	a = malloc(b);
-
-	if (a == NULL)
+	mem = malloc(b);
+	if (mem == NULL)
 		exit(98);
-	return (a);
+	return (mem);
 }
