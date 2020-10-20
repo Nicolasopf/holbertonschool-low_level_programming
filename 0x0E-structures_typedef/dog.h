d39345009f7460a31b6c4e42bd251a41b06709bb
root <root@ip-172-31-45-8.us-east-2.compute.internal>
2020-10-20 14:23:32 +0000

    type struct dog with three elements

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef _dog_h_
 #define _dog_h_
 
+/**
+ * struct dog - structure for dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
 struct dog
 {
 	char *name;
