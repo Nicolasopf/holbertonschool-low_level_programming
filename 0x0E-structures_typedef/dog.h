2368d9c5d55559a16991d515795d14fccde38696
root <root@ip-172-31-45-8.us-east-2.compute.internal>
2020-10-20 23:23:45 +0000

    Some prototypes of functions added

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,7 +8,7 @@
  * @owner: owner of the dog
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	char *owner;
@@ -17,5 +17,8 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 
 #endif /* _dog_h_ */
