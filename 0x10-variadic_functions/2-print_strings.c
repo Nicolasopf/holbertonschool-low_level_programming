9ff46cba49cf64bb0b7bbcc3d7537d66fa70f9bc
root <root@ip-172-31-45-8.us-east-2.compute.internal>
2020-10-25 19:21:50 +0000

    Added if separator NULL

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,6 +13,11 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list list;
 	va_start(list, n);
 
+	if (separator == NULL)
+	{
+		separator = "";
+	}
+
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(list, char *);
