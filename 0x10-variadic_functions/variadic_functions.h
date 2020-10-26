8523d0ff75ac251c272f3132ca4d165a357eb214
root <root@ip-172-31-45-8.us-east-2.compute.internal>
2020-10-26 04:19:21 +0000

    Struct added

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -10,4 +10,15 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+typedef struct type
+{
+	char *tp;
+	void (*f)();
+} type_t;
+
+void p_char(va_list a);
+void p_int(va_list a);
+void p_float(va_list a);
+void p_string(va_list a);
+
 #endif /* _variadic_functions_h_ */
