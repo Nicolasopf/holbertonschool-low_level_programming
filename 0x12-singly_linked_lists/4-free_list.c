52a5edb6d4bac9af4d22e7d95219b9ea2a59f468
Nicolasopf <root@ip-172-31-32-156.us-east-2.compute.internal>
2020-10-30 20:07:19 +0000

    Free a list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list
+ * @head: pointer to the list
+ */
+
+void free_list(list_t *head);
+{
+	if (head)
+	{
+		while (head)
+		{
+			free(head->str);
+			free(head);
+			head = head->next;
+		}
+	}
+}
