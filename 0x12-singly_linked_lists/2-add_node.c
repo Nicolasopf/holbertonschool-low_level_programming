02389a921573ab632d7ae25c43c36154938d4ea7
Nicolasopf <root@ip-172-31-32-156.us-east-2.compute.internal>
2020-10-30 21:57:54 +0000

    Defined value to len

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,18 +9,18 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	unsigned int i;
+	unsigned int len = 0;
 	list_t *nombre;
 
 	if (!head)
 		return (NULL);
-	while (str[i])
-		i++;
+	while (str[len])
+		len++;
 	nombre = malloc(sizeof(list_t));
 	if (!nombre)
 		return (NULL);
 	nombre->str = strdup(str);
-	nombre->len = i;
+	nombre->len = len;
 	nombre->next = *head;
 	*head = nombre;
 	return (*head);
