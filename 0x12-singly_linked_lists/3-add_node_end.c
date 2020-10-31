fa14a8ae097ead19376bd535d1cfc4477e06912c
root <root@DESKTOP-KUUNDHL.localdomain>
2020-10-30 22:03:10 -0500

    Spaces blank deleted

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,20 +9,21 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-        unsigned int len = 0;
-        list_t *nombre;
+	unsigned int len = 0;
+	list_t *nombre;
+
 	if (!head)
 		return (NULL);
-        nombre = *head;
-        while (nombre->next != NULL)
-                nombre = nombre->next;
-        while (str[len])
-                len++;
-        nombre = malloc(sizeof(list_t));
-        if (!nombre)
-                return (NULL);
-        nombre->str = strdup(str);
-        nombre->len = len;
-        nombre->next = NULL;
-        return (nombre);
+	nombre = *head;
+	while (nombre->next != NULL)
+		nombre = nombre->next;
+	while (str[len])
+		len++;
+	nombre = malloc(sizeof(list_t));
+	if (!nombre)
+		return (NULL);
+	nombre->str = strdup(str);
+	nombre->len = len;
+	nombre->next = NULL;
+	return (nombre);
 }
