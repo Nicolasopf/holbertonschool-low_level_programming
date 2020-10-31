427d6282f02dc92cf44710b41c252991610a5826
Nicolasopf <69660332+Nicolasopf@users.noreply.github.com>
2020-10-30 21:28:30 -0500

    Function that add a new node at the end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -7,19 +7,22 @@
  */
 
 list_t *add_node_end(list_t **head, const char *str)
+{list_t *add_node_end(list_t **head, const char *str)
 {
-	unsigned int len;
-
-	while (nombre->next != NULL)
-		nombre = nombre->next;
-	while (str[len])
-		len++;
-	if (!nombre)
+        unsigned int len = 0;
+        list_t *nombre;
+	if (!head)
 		return (NULL);
-	nombre = malloc(sizeof(list_t));
-	nombre->str = strdup(str);
-	nombre->len = len;
-	nombre->next = *head;
-	*head = nombre;
-	return (*head);
+        nombre = *head;
+        while (nombre->next != NULL)
+                nombre = nombre->next;
+        while (str[len])
+                len++;
+        nombre = malloc(sizeof(list_t));
+        if (!nombre)
+                return (NULL);
+        nombre->str = strdup(str);
+        nombre->len = len;
+        nombre->next = NULL;
+        return (nombre);
 }
