05c727d719d953919066b53049c6d9aed00520be
Nicolas <nico15935746@gmail.com>
2020-11-03 14:21:21 -0500

    comments added

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
- *
- *
- *
+ * print_listint - print the elements of a list
+ * @h: pointer to list
+ * Return: number of elements
  */
 
 size_t print_listint(const listint_t *h)
