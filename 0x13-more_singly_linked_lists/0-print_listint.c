48249f2ef3ab545fe76fbb18c3af9ab868692df8
Nicolas <nico15935746@gmail.com>
2020-11-04 01:52:31 -0500

    changed unsigned to size_t

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,16 +8,12 @@
 
 size_t print_listint(const listint_t *h)
 {
-	unsigned int a = 0;
-
-	if (h)
-	{
+	size_t a = 0;
 		while (h)
 		{
 			printf("%d\n", h->n);
 			h = h->next;
 			a++;
 		}
-	}
 	return (a);
 }
