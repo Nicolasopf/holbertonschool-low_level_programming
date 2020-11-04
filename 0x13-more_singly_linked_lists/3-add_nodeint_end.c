c93940ffd921def99c3093fcbba3546acfb1388b
Nicolas <nico15935746@gmail.com>
2020-11-04 17:13:37 -0500

    added last node

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - add a enw node at the end
+ * @head: Pointer to the head
+ * @n: element of list
+ * Return: the last node
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *last, *new = NULL;
+
+	last = *head;
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	if (*head)
+	{
+		while (*head->next != NULL)
+			*head = head->next;
+		last->next = new;
+	}
+	else
+		*head = new;
+	return (head);
+}
