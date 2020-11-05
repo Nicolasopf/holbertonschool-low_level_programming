585c07cc26103eceeba5253f06f09601298e914c
Nicolas <nico15935746@gmail.com>
2020-11-04 21:01:57 -0500

    Another way

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,9 +11,9 @@ void free_listint2(listint_t **head)
 
 	while (*head)
 	{
-		temp = (*head)->next;
-		free(*head);
-		*head = temp;
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
 	}
 	head = NULL;
 }
