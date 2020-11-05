4330a94925541331b348740572542babf4bc81d4
Nicolas <nico15935746@gmail.com>
2020-11-04 21:04:42 -0500

    if statement for head added

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,11 +9,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	while (*head)
+	if (head)
 	{
-		temp = *head;
-		*head = (*head)->next;
-		free(temp);
+		while (*head)
+		{
+			temp = *head;
+			*head = (*head)->next;
+			free(temp);
+		}
 	}
 	head = NULL;
 }
