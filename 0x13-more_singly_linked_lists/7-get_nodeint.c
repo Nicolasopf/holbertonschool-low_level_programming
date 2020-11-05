89661b1c55f718379ab08de49f7e87219769d30c
Nicolas <nico15935746@gmail.com>
2020-11-04 23:32:42 -0500

    Added comments and changed while to for

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- *
- *
- *
- *
+ * get_nodeint_at_index - print the node of the index
+ * @head: pointer to head
+ * @index: index of the node
+ * Return: Return the head
  */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
@@ -13,7 +13,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	if (head)
 	{
-		while (i < index)
+		for (i = 0; i < index; i++)
 		{
 			head = head->next;
 			if (!head)
