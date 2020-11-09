1042583b16f5ab7d2783ad7850f789f71885758f
Nicolas <nico15935746@gmail.com>
2020-11-09 18:19:35 -0500

    Creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * create_file - Function that creates a file
+ * @filename: Name of the file
+ * @text_content: content of the text
+ * Return: 1 if works, -1 if not.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int len = 0, fi, rw;
+
+	if (filename)
+	{
+		file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 384);
+		if (fi == -1)
+			return (-1);
+		if (text_content && text_content[len])
+		{
+			while (text_content[len])
+				len++;
+			rw = write(fi, text_content, len);
+			if (rw == -1)
+				return (-1);
+		}
+		close(fi);
+		return (1);
+	}
+	return (-1);
+}
