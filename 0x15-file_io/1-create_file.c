7b08efb060690686956d6f56adbcbfc542e488f8
Nicolas <nico15935746@gmail.com>
2020-11-09 18:41:01 -0500

    changed file to fi

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename)
 	{
-		file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 384);
+		fi = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 384);
 		if (fi == -1)
 			return (-1);
 		if (text_content && text_content[len])
