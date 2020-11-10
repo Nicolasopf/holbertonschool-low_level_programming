513a5c309cc72aaab12296ce5337e117dbea3a52
Nicolas <nico15935746@gmail.com>
2020-11-09 21:29:41 -0500

    Changed function

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 
 /**
- * create_file - Function that creates a file
+ * append_text_to_file - appends text to the end of a file
  * @filename: Name of the file
  * @text_content: content of the text
  * Return: 1 if works, -1 if not.
  */
 
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int len = 0, fi, rw;
 
