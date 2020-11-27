c953a20fb9fd5c0af1d927c07640647912a47c26
Nicolas <nico15935746@gmail.com>
2020-11-26 22:20:53 -0500

    Changed unsigned int to int

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,18 +14,9 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(list, n);
 
-	if (n == 0)
-	{
-		return (0);
-	}
-	else
-	{
-		for (i = 0; i < n; i++)
-		{
-			sum += va_arg(list, unsigned int);
-		}
-	}
-	va_end(list);
+	for (i = 0; i < n; i++)
+		sum += va_arg(list, int);
 
+	va_end(list);
 	return (sum);
 }
