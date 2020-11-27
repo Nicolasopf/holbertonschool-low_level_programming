f571b816dd83bdc741e06348ae6163cb6472aeb7
Nicolas <nico15935746@gmail.com>
2020-11-26 20:12:28 -0500

    prints a buffer

---
+++ 0x06-pointers_arrays_strings/103-print_buffer.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * print_buffer - prints a buffer.
+ * @b: string to encode.
+ * @size: size bytes of the buffer pointed.
+ */
+
+void print_buffer(char *b, int size)
+{
+	a = 1;
+	_putchar(a);
+}
