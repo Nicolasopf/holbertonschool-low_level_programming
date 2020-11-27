bc73b5b66da8b58214e9e069514a050736e344a4
Nicolas <nico15935746@gmail.com>
2020-11-26 20:29:50 -0500

    Some prototypes added

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -3,7 +3,9 @@
 
 int _putchar(char c);
 void print_alphabet(void);
+void last(int x, int n);
 void print_alphabet_x10(void);
+void print_times_table(int n)
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
