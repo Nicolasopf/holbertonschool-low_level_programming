707767e5bacabef22e34306f7911b57e94411b1b
Nicolas <nico15935746@gmail.com>
2020-12-13 01:56:24 -0500

    OR operator added for head

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -14,7 +14,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
 		return (NULL);
-	if (head == NULL && *head == NULL)
+	if (head == NULL || *head == NULL)
 		return (addd_dnodeint(head, n));
 	last = *head;
 	while (last)
