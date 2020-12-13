15a380c5447cdab50a7ef9863adae48d4a413a26
Nicolas <nico15935746@gmail.com>
2020-12-13 16:53:54 -0500

    New edge cases added

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,6 +13,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *tmp = *h, *new;
 	unsigned int i = 0;
 
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 	new = malloc(sizeof(dlistint_t));
 	while (i < idx - 1)
 	{
