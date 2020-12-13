99240f0c909cde0d55a59a4c4e18af97f7e8a51f
Nicolas <nico15935746@gmail.com>
2020-12-13 18:15:43 -0500

    New

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -13,7 +13,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *tmp = *head;
 	unsigned int i = 0;
 
-	if (!*head)
+	if (!*head || !head)
 		return (-1);
 	if (index == 0)
 	{
