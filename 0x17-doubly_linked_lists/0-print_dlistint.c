0bf5b507d086dfba050367d08de449bbdf956ca9
Nicolas <nico15935746@gmail.com>
2020-12-13 00:56:17 -0500

    Betty fixed

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_dlistint - Print a list. 
+ * print_dlistint - Print a list.
  * @h: Header.
  * Return: Num of elements
  */
@@ -9,6 +9,7 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	unsigned int a = 0;
+
 	while (h)
 	{
 		printf("%d\n", h->n);
