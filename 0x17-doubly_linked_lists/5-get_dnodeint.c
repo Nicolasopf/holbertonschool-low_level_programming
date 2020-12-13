315676db2f7ae37acc72067dac4628c766490c58
Nicolas <nico15935746@gmail.com>
2020-12-13 15:41:00 -0500

    If

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -12,14 +12,14 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	dlistint_t tmp = head;
 	unsigned int i;
 
-	if (!head)
-		return (NULL);
-
-	for (i = 0; i < index; i++)
+	if (head)
 	{
-		tmp = tmp->head;
-		if (!tmp)
-			return (NULL);
+		for (i = 0; i < index; i++)
+		{
+			tmp = tmp->head;
+			if (!tmp)
+				return (NULL);
+		}
 	}
 	return (tmp);
 }
