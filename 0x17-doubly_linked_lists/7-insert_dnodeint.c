a970bfb8fe03238dd97910c8fcb1587e4ea54472
Nicolas <nico15935746@gmail.com>
2020-12-13 16:48:12 -0500

    Inserts a new node without edge cases

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - Insert a node at specific index.
+ * @h: head of the list.
+ * @n: Number passed for the node.
+ * @idx: Index of the node
+ * Return: The new node at specific position or NULL.
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *tmp = *h, *new;
+	unsigned int i = 0;
+
+	new = malloc(sizeof(dlistint_t));
+	while (i < idx - 1)
+	{
+		if (!tmp)
+			return (NULL);
+		tmp = tmp->next;
+		i++;
+	}
+	new->n = n;
+	if (tmp->next)
+		new->next = tmp->next;
+	else
+		new->next = NULL;
+	tmp->next->prev = new;
+	tmp->next = new;
+	new->prev = tmp;
+
+	return (new);
+}
