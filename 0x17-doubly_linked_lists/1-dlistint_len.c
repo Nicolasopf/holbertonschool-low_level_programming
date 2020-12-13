f7cafe4e73e8385c6c24f91e96eb1215b3ac94fc
Nicolas <nico15935746@gmail.com>
2020-12-13 00:40:59 -0500

    Prints the length

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - Prints the linked list length
+ * @h: Head
+ * Return: Num of elements
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	unsigned int a;
+
+	while (h)
+	{
+		h = h->next;
+		a++;
+	}
+	return (a);
+}
