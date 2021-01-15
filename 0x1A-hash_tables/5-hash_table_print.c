75d4802a9724bcebd22714d4d6e9e66464aee2fc
Nicolas <nico15935746@gmail.com>
2021-01-15 17:10:38 -0500

    Prints a hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Prints a hash table.
+ * @ht: Hash table to look into.
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *tmp;
+	unsigned long int idx = 0;
+	int tmpp = 0;
+
+	if (!ht)
+		return;
+	printf("{");
+	while (idx < ht->size)
+	{
+		tmp = ht->array[idx];
+		while (tmp)
+		{
+			if (tmpp > 0)
+				printf(", ");
+			printf("'%s' : '%s'", tmp->key, tmp->value);
+			tmp = tmp->next;
+			tmpp++;
+		}
+		idx++;
+	}
+	printf("}\n");
+}
