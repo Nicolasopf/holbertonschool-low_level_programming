774a2e828459a479b8a3d0641e96d2c021b55660
Nicolas <nico15935746@gmail.com>
2021-02-19 16:55:45 -0500

    Added comment for def perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,14 +4,18 @@
 
 def island_perimeter(grid):
     """ Return the perimeter of an island """
-    f = 0
-    h = 0
-    for row in range(len(grid)):
-        for c in range(len(grid[row])):
-            if grid[row][c] == 1 and grid[row+1][c]:
-                h += 1
-            elif grid[row][c] == 1 and grid[row][c+1]:
-                f += 1
-    h += 1
-    f += 1
-    return (f+h) * 2
+    # f = 0
+    # h = 0
+    # for row in range(len(grid)):
+    #     for c in range(len(grid[row])):
+    #         if grid[row][c] == 1 and grid[row+1][c]:
+    #             h += 1
+    #         elif grid[row][c] == 1 and grid[row][c+1]:
+    #             f += 1
+    # h += 1
+    # f += 1
+    # return (f+h) * 2
+
+    x = grid.copy()
+    x(filter(lambda a: a != 0, x))
+    return x
