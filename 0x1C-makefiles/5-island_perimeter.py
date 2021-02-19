9d743abc8bf46ee71b2e9240b32de7bdeafde14b
Nicolas <nico15935746@gmail.com>
2021-02-19 16:50:31 -0500

    Other way using a form to check rows and columns

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,12 +3,14 @@
 
 
 def island_perimeter(grid):
-    f = 0, h = 0
+    f = 0
+    h = 0
     for row in range(len(grid)):
         for c in range(len(grid[row])):
-            if grid[row][c] == 1:
+            if grid[row][c] == 1 and grid[row+1][c]:
+                h += 1
+            elif grid[row][c] == 1 and grid[row][c+1]:
                 f += 1
-        if grid[row] == 1:
-            h += 1
+    h += 1
     f += 1
-    return f * 2
+    return (f+h) * 2
